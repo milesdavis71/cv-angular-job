6381b8637f373bd3ac43c422257fd4b16db00e43
unknown <hevesiistvan71@hotmail.com>
2022-03-25 12:52:43 +0100

    Initialise RenderValidation class.

---
+++ src/app.ts
@@ -0,0 +1,18 @@
+class RenderEnterButton {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      "validation"
+    )! as HTMLTemplateElement;
+    this.hostElement = document.getElementById("app")! as HTMLDivElement;
+
+    const importNode = document.importNode(this.templateElement.content, true);
+    this.element = importNode.firstElementChild as HTMLFormElement;
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
+  }
+}
+
+const EnterBtn = new RenderEnterButton();
